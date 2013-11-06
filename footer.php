1b025cda7f4ad170c7f4baba89e899f04a24a315
ssalazarSU <ssalazar@salazar-pc.home>
2013-11-05 22:34:12 -0800

    Updated footer

--- footer.php
+++ footer.php
@@ -5,13 +5,31 @@
       
 
       <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <footer class='page-footer'>
+        <p class='text-center'><small>&copy; <?php echo date('Y'); ?>
+	   &nbsp;Claremont&nbsp;Graduate&nbsp;University
+	   <img src="<?php echo get_template_directory_uri(); ?>/images/footer/bulletFooter.gif">
+	   &nbsp;150&nbsp;E.&nbsp;10th&nbsp;St.,&nbsp;Claremont,&nbsp;CA&nbsp;91711
+	   <img src="<?php echo get_template_directory_uri(); ?>/images/footer/bulletFooter.gif">
+	   &nbsp;(909)&nbsp;621-8000 
+	   <img src="<?php echo get_template_directory_uri(); ?>/images/footer/bulletFooter.gif">
+	   &nbsp;
+	   <a href="http://www.cuc.claremont.edu/campussafety/">Campus&nbsp;Safety</a> 
+	   <img src="<?php echo get_template_directory_uri(); ?>/images/footer/bulletFooter.gif">
+	   &nbsp;
+	   <a href="http://cguemergency.info/">Emergency&nbsp;Info</a>
+	    <img src="<?php echo get_template_directory_uri(); ?>/images/footer/bulletFooter.gif">
+	   &nbsp;
+     	   <a href="http://www.cgu.edu/pages/1418.asp">Campus&nbsp;Map/Driving&nbsp;Directions</a>
+	   </small> 
+	</p>
+	<p class='text-center'>
+	   <a href="#">Back to top</a>
+	</p>
       </footer>
-
+     
     </div><!-- /.container -->
-
+    <br>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
